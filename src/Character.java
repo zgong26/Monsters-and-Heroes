686cac55845bf700c83709520cc5a5f3a0b2795a
zgong26 <79713887+zgong26@users.noreply.github.com>
2022-11-05 15:35:36 -0400

    update

---
+++ src/Character.java
@@ -0,0 +1,7 @@
+public abstract class Character {
+    protected String name;
+    protected int level;
+    protected int HP;
+    protected double strength;
+    
+}
---
+++ src/Hero.java
@@ -0,0 +1,12 @@
+public class Hero {
+    private String name;
+    private int level;
+    private int HP;
+    private double strength;
+    private double dexterity;
+    private double agility;
+    private int gold;
+    //private item?
+
+
+}
